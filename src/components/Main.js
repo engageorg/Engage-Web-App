7c05b39a1c769836d149e690fc5d25951c8c6d91
Aman <theamanamanaman@gmail.com>
2021-08-28 10:16:25 +0530

    formatting corrected and bit of comments added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,104 +1,101 @@
-import React from 'react';
-import useLocalStorage from '../hooks/useLocalStorage'
-
+import React from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 export default function App() {
+  const Recording = { events: [], startTime: -1 };
+  const [rec, setrec] = useLocalStorage("recording", Recording);
 
-  const Recording = { events: [], startTime: -1 }; 
-  const [ rec, setrec] = useLocalStorage('recording',  Recording);
+  // Record each type of event
+  const handlers = [
+    {
+      eventName: "mousemove",
+      handler: function handleMouseMove(e) {
+        Recording.events.push({
+          type: "mousemove",
+          x: e.pageX,
+          y: e.pageY,
+          time: Date.now(),
+        });
+      },
+    },
+    {
+      eventName: "click",
+      handler: function handleClick(e) {
+        Recording.events.push({
+          type: "click",
+          target: e.target.className,
+          x: e.pageX,
+          y: e.pageY,
+          time: Date.now(),
+        });
+      },
+    },
+    {
+      eventName: "keypress",
+      handler: function handleKeyPress(e) {
+        Recording.events.push({
+          type: "keypress",
+          target: e.target.className,
+          value: e.target.value,
+          keyCode: e.keyCode,
+          time: Date.now(),
+        });
+      },
+    },
+  ];
 
-    // Record each type of event
-    const handlers = [
-        {
-        eventName: "mousemove",
-        handler: function handleMouseMove(e) {
-            Recording.events.push({
-            type: "mousemove",
-            x: e.pageX,
-            y: e.pageY,
-            time: Date.now()
-            });
-        }
-        },
-        {
-        eventName: "click",
-        handler: function handleClick(e) {
-            Recording.events.push({
-            type: "click",
-            target: e.target.className,
-            x: e.pageX,
-            y: e.pageY,
-            time: Date.now()
-            });
-        }
-        },
-        {
-        eventName: "keypress",
-        handler: function handleKeyPress(e) {
-            Recording.events.push({
-            type: "keypress",
-            target: e.target.className,
-            value: e.target.value,
-            keyCode: e.keyCode,
-            time: Date.now()
-            });
-        }
-        }
-    ];
+  function listen(eventName, handler) {
+    return document.documentElement.addEventListener(eventName, handler, true);
+  }
 
-    function listen(eventName, handler) {
-        return document.documentElement.addEventListener(eventName, handler, true);
-    }
-    
-    function startRecording() {
+  function startRecording() {
+    Recording.startTime = Date.now();
+    Recording.events = [];
+    handlers.map((x) => listen(x.eventName, x.handler));
+  }
 
-        Recording.startTime = Date.now();
-        Recording.events = [];
-        handlers.map(x => listen(x.eventName, x.handler));
-    
-    }
+  function removeListener(eventName, handler) {
+    return document.documentElement.removeEventListener(
+      eventName,
+      handler,
+      true
+    );
+  }
 
-    function removeListener(eventName, handler) {
-        return document.documentElement.removeEventListener(
-          eventName,
-          handler,
-          true
-        );
-    }
+  function handleClick(e) {
+    e.preventDefault();
+    startRecording();
+    console.log("The Button was clicked.");
+  }
 
-    function handleClick(e) {
-        e.preventDefault();
-        startRecording();
-        console.log('The Button was clicked.');
-    
-    }
+  function stopRecording() {
+    // stop recording
+    handlers.map((x) => removeListener(x.eventName, x.handler));
+    setrec(Recording);
+  }
 
-    function stopRecording() {
-        // stop recording
-        handlers.map(x => removeListener(x.eventName, x.handler));
-        setrec(Recording);
-        
-      }
+  function handleStop(e) {
+    e.preventDefault();
+    stopRecording();
+    console.log("Recording Stopped.");
+  }
 
-    function handleStop(e) {
-        e.preventDefault();
-        stopRecording();
-        console.log('Recording Stopped.');
-    }
- 
-    return (
-        <>
-        <div className = "heading">test</div>
-        <br></br>
-        <input className = "input1"></input>
-        
-        <input className = "input2"></input>
-        
-        <input className = "input3"></input>
+  return (
+    <>
+      <div className="heading">test</div>
+      <br></br>
+      <input className="input1"></input>
 
-        <button onClick={handleClick} className="record">Start Record</button>
-        <button onClick={handleStop} className = "button" id="record">Stop Recording</button>
+      <input className="input2"></input>
 
-        </>
-    )
+      <input className="input3"></input>
+
+      <button onClick={handleClick} className="record">
+        Start Record
+      </button>
+      <button onClick={handleStop} className="button" id="record">
+        Stop Recording
+      </button>
+    </>
+  );
 }
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,84 +1,94 @@
-import React, { useState, useEffect } from 'react';
-import useLocalStorage from '../hooks/useLocalStorage'
-
+import React, { useEffect } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 export default function Video() {
+  const [rec, setrec] = useLocalStorage("recording", "");
 
-    const [rec, setrec] = useLocalStorage('recording', '');
-    //console.log(rec);
- 
-    useEffect(() => {
-    const fakeCursor = document.createElement('div');
+  useEffect(() => {
+    // fake cursor, declared outside, so it will scoped to all functions
+    const fakeCursor = document.createElement("div");
     fakeCursor.className = "cursor";
+
+    //when user clicked playbutton
     var play = document.getElementById("play");
-    let i = 0;
-    play.addEventListener("click",function(){ 
-                  document.getElementById("root").appendChild(fakeCursor);
-                  const startPlay = Date.now();
 
-                  var doc = document.documentElement;
-                  (function draw(){
-                    let event = rec.events[i];
-                    if (!event) {
-                      return;
-                    }
-                    let offsetRecording = event.time - rec.startTime;
-                    let offsetPlay = (Date.now() - startPlay) * 1;
-                    if (offsetPlay >= offsetRecording) {
-                      drawEvent(event, fakeCursor, doc);
-                      i++;
-                    }   
-                    
-                    if(i < rec.events.length){
-                      requestAnimationFrame(draw);
-                    }
-                  })();
-      })
+    play.addEventListener("click", function () {
+      let i = 0;
+      //append fake cursor when user clicks play button
+      document.getElementById("root").appendChild(fakeCursor);
+      const startPlay = Date.now();
 
-     
+      //draw event to play all events in requestAnimationFrames
+      var documentReference = document.documentElement;
+      (function draw() {
+        //select an event and check if its empty
+        let event = rec.events[i];
+        if (!event) {
+          return;
+        }
 
-          function drawEvent(event, fakeCursor, Doc) {
-            if (event.type === "click" || event.type === "mousemove") {
-                console.log("mouse");
-                document.getElementsByClassName("cursor")[0].style.top = JSON.stringify(event.y) + "px"
-                fakeCursor.style.left = JSON.stringify(event.x) + "px"
-                console.log("y:", fakeCursor.style);  
-            }
-            if (event.type === "click") {
-              console.log("mouseclick");
-              flashClass(fakeCursor, "click");
-              console.log(event.target);
-              var tar = document.getElementsByClassName(event.target)[0];
-              flashClass(tar, "clicked");
-            }
-            if (event.type === "keypress") {
-              console.log("keypress");
-              const path = event.target;
-              var tar = document.getElementsByClassName(path)[0];
-              tar.focus();
-              tar.value = event.value;
-            } 
-          }
-    
-          function flashClass(el, className) {
-            el.classList.add(className);
-            setTimeout(function(){ el.classList.remove(className); }, 200);
-          }
-    })
+        //To check if event is valid
+        let offsetRecording = event.time - rec.startTime;
+        let offsetPlay = (Date.now() - startPlay) * 1;
+        if (offsetPlay >= offsetRecording) {
+          //draws event amd matches with listner
+          drawEvent(event, fakeCursor, documentReference);
+          i++;
+        }
 
-    return(
-        <>
-        <div className = "heading">test</div>
-        
-        <input className = "input1"></input>
-        
-        <input className = "input2"></input>
-        
-        <input className = "input3"></input>
+        //animates in avg frame rate (60 fps mostly) of display, so motion is smooth(tells the browser that animation needs to happen)
+        if (i < rec.events.length) {
+          requestAnimationFrame(draw);
+        }
+      })();
+    });
 
-        <button  className="record">Start Record</button>
-        <button  className = "button" id="record">Stop Recording</button>
-        <button id = "play">Play</button>
-        </>
-    )
+    function drawEvent(event, fakeCursor, documentReference) {
+      if (event.type === "click" || event.type === "mousemove") {
+        console.log("mouse");
+        document.getElementsByClassName("cursor")[0].style.top =
+          JSON.stringify(event.y) + "px";
+        fakeCursor.style.left = JSON.stringify(event.x) + "px";
+      }
+      if (event.type === "click") {
+        console.log("mouseclick");
+        flashClass(fakeCursor, "click");
+        console.log(event.target);
+        var tar = document.getElementsByClassName(event.target)[0];
+        flashClass(tar, "clicked");
+      }
+      if (event.type === "keypress") {
+        console.log("keypress");
+        const path = event.target;
+        var tar = document.getElementsByClassName(path)[0];
+        tar.focus();
+        tar.value = event.value;
+      }
+    }
+
+    function flashClass(el, className) {
+      el.classList.add(className);
+      setTimeout(function () {
+        el.classList.remove(className);
+      }, 200);
+    }
+  });
+
+  return (
+    <>
+      <div className="heading">test</div>
+
+      <input className="input1"></input>
+
+      <input className="input2"></input>
+
+      <input className="input3"></input>
+
+      <button className="record">Start Record</button>
+      <button className="button" id="record">
+        Stop Recording
+      </button>
+      <button id="play">Play</button>
+    </>
+  );
 }
