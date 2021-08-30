1cb2a30b0cef19390ed8d0f346a6602c01cf2058
shantanu <56212958+cigar-galaxy82@users.noreply.github.com>
2021-08-30 08:38:36 +0530

    added play and pause feature

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,11 +1,12 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import IDE from "./IDE";
 import { useSelector, useDispatch } from "react-redux";
-import { js, css, html } from '../actions'
+import { js, css, html } from "../actions";
 import files from "../assets/files";
 
-
 export default function Video() {
+  const [valueCode, setValue] = useState("");
+  const [previousValueCode, setPreviousValue] = useState("");
 
   const [keyCode, setKeycode] = useState('');
 
@@ -13,50 +14,73 @@ export default function Video() {
   const dispatch = useDispatch();
 
   const file = files[fileName];
-   
+  const fakeCursor = document.createElement("div");
+  document.getElementById("root").appendChild(fakeCursor);
+  fakeCursor.style.display='none'
   // fetch recording from local storage
   let recording = { events: [], startTime: -1 };
-  const recordingJsonValue = localStorage.getItem("recording")
-  if (recordingJsonValue != null) recording = JSON.parse(recordingJsonValue)
-  console.log(recording);
-
+  const recordingJsonValue = localStorage.getItem("recording");
+  if (recordingJsonValue != null) recording = JSON.parse(recordingJsonValue);
+  //console.log(recording);
+  let playLecture = 1;
+  let pauseLecture = 0;
   useEffect(() => {
+    console.log("THIS")
     // fake cursor, declared outside, so it will scoped to all functions
-    const fakeCursor = document.createElement("div");
-    fakeCursor.className = "cursor";
+    fakeCursor.className = "customCursor";
 
     //when user clicked playbutton
-    var play = document.getElementById("play");
+    const play = document.getElementById("play");
+    const pause = document.getElementById("pause");
+    let i = 0;
+
+    pause.addEventListener("click", function () {
+      //setPlay(0)
+      console.log("Pause Button Clicked")
+      //playLecture !== 0 ? 0: 1;
+      //if(playLecture!==0){
+        playLecture = 0;
+        fakeCursor.style.display = 'none'
+      //}
+    });
 
     play.addEventListener("click", function () {
-      let i = 0;
+      //setPlay(1)
       //append fake cursor when user clicks play button
-      document.getElementById("root").appendChild(fakeCursor);
+      console.log("Play button click")
+      fakeCursor.style.display = 'block'
       const startPlay = Date.now();
-
+      playLecture = 1;
       //draw event to play all events in requestAnimationFrames
       var documentReference = document.documentElement;
+      //if(playLecture === 1){
       (function draw() {
-        //select an event and check if its empty
-        let event = recording.events[i];
-        if (!event) {
-          return;
-        }
+        if (playLecture === 1) {
+          //select an event and check if its empty
+          let event = recording.events[i];
+          //console.log(event);
+          if (!event) {
+            return;
+          }
 
-        //To check if event is valid
-        let offsetRecording = event.time - recording.startTime;
-        let offsetPlay = (Date.now() - startPlay) * 1;
-        if (offsetPlay >= offsetRecording) {
-          //draws event amd matches with listner
-          drawEvent(event, fakeCursor, documentReference);
-          i++;
-        }
+          //To check if event is valid
+          let offsetRecording = event.time - recording.startTime;
+          let offsetPlay = (Date.now() - startPlay) * 1;
+          if (offsetPlay >= offsetRecording) {
+            //draws event amd matches with listner
+            //console.log(playLecture);
+            //console.log(pauseLecture);
+            drawEvent(event, fakeCursor, documentReference);
+            i++;
+          }
 
-        //animates in avg frame rate (60 fps mostly) of display, so motion is smooth(tells the browser that animation needs to happen)
-        if (i < recording.events.length) {
-          requestAnimationFrame(draw);
+          //animates in avg frame rate (60 fps mostly) of display, so motion is smooth(tells the browser that animation needs to happen)
+          if (i < recording.events.length) {
+            requestAnimationFrame(draw);
+          }
         }
       })();
+      //}
     });
     
     function handleButtonEvents(target) {
@@ -85,20 +109,17 @@ export default function Video() {
       if (event.type === "click") {
         console.log("mouseclick");
         flashClass(fakeCursor, "click");
-        console.log(event.target);
+        //console.log(event.target);
         var tar = document.getElementsByClassName(event.target)[0];
         if(tar !=  null){
           handleButtonEvents(tar.className);
           flashClass(tar, "clicked");
         }
-       
       }
       if (event.type === "keypress") {
-        // console.log("keypress");
-        // console.log(event.value)
         const path = event.target;
         var tar = document.getElementsByClassName(path)[0];
-        if(tar != null){
+        if (tar != null) {
           tar.focus();
           setKeycode(keyCode => keyCode + String.fromCharCode(event.keyCode));
         }
@@ -111,7 +132,7 @@ export default function Video() {
         el.classList.remove(className);
       }, 200);
     }
-  });
+  }, []);
 
   return (
     <>
@@ -121,6 +142,9 @@ export default function Video() {
         Stop Recording
       </button>
       <button id="play">Play</button>
+      <button id="pause">Pause</button>
+      {/* <button>{playLecture}</button>
+      <button>{pauseLecture}</button> */}
     </>
   );
 }
