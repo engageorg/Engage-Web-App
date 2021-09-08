17b2270d279f296393c507a1e8bb2e45e6f7fb4b
Aman <theamanamanaman@gmail.com>
2021-09-08 20:23:49 +0530

    disabled in phone

--- src/components/IDE.js
+++ src/components/IDE.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import TextEditor from './TextEditor';
+import { Link } from 'react-router-dom';
 
 function App(props) {
   return (
+    <>
+    <Link to="/videoplayer">videoplayer test</Link>
     <TextEditor 
      value = {props.val}
      parentCallBack = {props.parentCallBack}/>
+    </>
   )
 }
 
