d8f00a9d02e99be185a9cf31cf95730d15268f91
shantanu69 <56212958+shantanu69@users.noreply.github.com>
2021-09-22 23:17:26 +0530

    removed proxy from hosted branch

--- src/actions/outputAction.js
+++ src/actions/outputAction.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const runCode = (language, code, input) => async dispatch => {
-    return await axios.post('/', {
+    return await axios.post('https://api.jdoodle.com/v1/execute', {
         clientId: "92ed4b582caf3c545e22d2c5ab336568",
         clientSecret: "b530d3b1ce3b93a0ac4f745f074c8f0ea3c86a04e72ba4ddd62e36516acff521",
         language: language, 
