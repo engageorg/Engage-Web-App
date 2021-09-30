f3c49daa12bb80a20b3caf683408da3b8944afff
cigar-galaxy82 <shantanushukla64@gmail.com>
2021-09-30 09:46:58 +0530

    added pencil tool in excaliClone and it's working

--- src/components/excaliClone/excaliClone.js
+++ src/components/excaliClone/excaliClone.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useLayoutEffect, useState} from 'react';
 import rough from 'roughjs/bundled/rough.esm'
-import { getStroke } from "perfect-freehand";
+import getStroke from 'perfect-freehand';
 import "./style.css";
 
 //A generator is a readonly property that lets you create a drawable object for a shape that can be later used with the draw method.
@@ -58,32 +58,57 @@ function ExcaliClone() {
 
   function createElement(id, x1,y1,x2,y2,type, fill="transparent", fillStyle="solid", stroke="black", strokeWidth=2) {
     let roughElement
-    if(type === "line"){
-      roughElement = generator.line(x1,y1,x2,y2, {strokeWidth:strokeWidth, stroke:stroke})
-    }
-    else if(type==="rectangle"){
-      roughElement = generator.rectangle(x1,y1,x2-x1,y2-y1, {fill: fill, fillStyle:fillStyle, stroke:stroke, strokeWidth:strokeWidth })
-    }
-    else if(type === "circle"){
-      const a = {x:x1, y:y1}
-      const b = {x:x2, y:y2}
-      const diameter = distance(a,b)
-      roughElement = generator.circle((x1+x2)/2,(y1+y2)/2,diameter, {fill:fill, fillStyle:fillStyle, stroke:stroke, strokeWidth:strokeWidth})
-    }else if(type==="ellipse"){
-      const a = {x:x1, y:y1}
-      const b = {x:x2, y:y2}
-      const height = distance(a,b)
-      const width = height/2
-      roughElement = generator.ellipse((x1+x2)/2,(y1+y2)/2, height,width )
-    }
-    return {id, x1, y1, x2, y2, type,roughElement}
+    switch (type) {
+      case "line":
+        roughElement = generator.line(x1,y1,x2,y2, {strokeWidth:strokeWidth, stroke:stroke})
+        return {id, x1, y1, x2, y2, type,roughElement}
+        case "rectangle":
+        roughElement = generator.rectangle(x1,y1,x2-x1,y2-y1, {fill: fill, fillStyle:fillStyle, stroke:stroke, strokeWidth:strokeWidth })
+        return {id, x1, y1, x2, y2, type,roughElement}
+        case "circle":
+        const a = {x:x1, y:y1}
+        const b = {x:x2, y:y2}
+        const diameter = distance(a,b)
+        roughElement = generator.circle((x1+x2)/2,(y1+y2)/2,diameter, {fill:fill, fillStyle:fillStyle, stroke:stroke, strokeWidth:strokeWidth})
+        return {id, x1, y1, x2, y2, type,roughElement}
+        case "pencil":
+        return {id,type, points:[{x:x1,y:y1}]}
+      default:
+        throw new Error(`Type not recognised`)
+      }
+    // if(type === "line"){
+    //   roughElement = generator.line(x1,y1,x2,y2, {strokeWidth:strokeWidth, stroke:stroke})
+    // }
+    // else if(type==="rectangle"){
+    //   roughElement = generator.rectangle(x1,y1,x2-x1,y2-y1, {fill: fill, fillStyle:fillStyle, stroke:stroke, strokeWidth:strokeWidth })
+    // }
+    // else if(type === "circle"){
+    //   const a = {x:x1, y:y1}
+    //   const b = {x:x2, y:y2}
+    //   const diameter = distance(a,b)
+    //   roughElement = generator.circle((x1+x2)/2,(y1+y2)/2,diameter, {fill:fill, fillStyle:fillStyle, stroke:stroke, strokeWidth:strokeWidth})
+    // }
+    // return {id, x1, y1, x2, y2, type,roughElement}
   }
 
   const updateElement = (id,x1,y1, x2, y2, type, fill, fillStyle, stroke,strokeWidth) => {
-    const updatedElement = createElement(id,x1,y1,x2,y2,type, fill, fillStyle, stroke,strokeWidth)
-    
+    // const updatedElement = createElement(id,x1,y1,x2,y2,type, fill, fillStyle, stroke,strokeWidth)
+    console.log(type)
     const elementsCopy = [...elements]
-    elementsCopy[id] = updatedElement
+    switch(type){
+      case "line":
+      case "rectangle":
+      case "circle":
+        const updatedElement = createElement(id,x1,y1,x2,y2,type, fill, fillStyle, stroke,strokeWidth)
+        elementsCopy[id] = updatedElement
+        break;
+      case "pencil":
+        //when updating for pencil copy the previous points and add new ones to assign them to points object
+        elementsCopy[id].points  = [...elementsCopy[id].points, {x:x2,y:y2}]
+        break;
+      default:
+        throw new Error('Type not recognised') 
+    }
     setElementState(elementsCopy)
   }
 
@@ -93,17 +118,21 @@ function ExcaliClone() {
     return Math.abs(x-x1) < 5 && Math.abs(y-y1) < 5 ? name : null 
   }
 
+  const onLine = (x1,y1,x2,y2,x,y, maxDistance=1) => {
+    const a = {x:x1, y:y1}
+    const b = {x:x2, y:y2}
+    const c = {x, y}
+    const offset = distance(a,b) - (distance(a,c) + distance(b,c))
+    return Math.abs(offset) < maxDistance ? "inside" : null
+  }
+
   const positionWithinElement = (x,y,element) => {
     const {type,x1,x2,y1,y2} = element
     if(type === "line"){
-      const a = {x:x1, y:y1}
-      const b = {x:x2, y:y2}
-      const c = {x, y}
-      const offset = distance(a,b) - (distance(a,c) + distance(b,c))
+      const on = onLine(x1,y1,x2,y2,x,y)
       const start = nearPoint(x,y,x1,y1,"start")
       const end = nearPoint(x,y,x2,y2, "end")
-      const inside = Math.abs(offset) < 1 ? 'inside':null
-      return start || end || inside
+      return start || end || on
     }else if(type === "rectangle"){
       const topLeft = nearPoint(x, y, x1, y1, "tl");
       const topRight = nearPoint(x, y, x2, y1, "tr");
@@ -127,6 +156,14 @@ function ExcaliClone() {
       }else{
         return null
       }
+    }else if(type==="pencil"){
+      const betweenAnyPoint = element.points.some((point, index) => {
+        const nextPoint = element.points[index+1]
+        if(!nextPoint) return false // if only one point
+        //think of the two points forming a line then check how far is the user click
+        return onLine(point.x, point.y,nextPoint.x,nextPoint.y, x,y) !== null
+      })
+      return betweenAnyPoint ?"inside": null
     }
   }
 
@@ -154,17 +191,50 @@ function ExcaliClone() {
     }
   }
 
-  const stroke = getStroke(points, options);
-  const pathData = getSvgPathFromStroke(stroke);
+    //The function below will turn the points returned by getStroke into SVG path data.
+    const getSvgPathFromStroke = (stroke) => {
+      if (!stroke.length) return ''
+    
+      const d = stroke.reduce(
+        (acc, [x0, y0], i, arr) => {
+          const [x1, y1] = arr[(i + 1) % arr.length]
+          acc.push(x0, y0, (x0 + x1) / 2, (y0 + y1) / 2)
+          return acc
+        },
+        ['M', ...stroke[0], 'Q']
+      )
+    
+      d.push('Z')
+      return d.join(' ')
+    }
+
+  function drawElement(roughCanvas, context, element) {
+    switch (element.type) {
+      case "line":
+      case "rectangle":
+      case "circle":
+        roughCanvas.draw(element.roughElement)
+        break
+        case "pencil":
+          //getStroke returns an array of points representing the outline of a stroke
+          const stroke = getSvgPathFromStroke(getStroke(element.points, {
+            size:5
+          }))
+          //The Path2D interface of the Canvas 2D API is used to declare a path that can then be used on a CanvasRenderingContext2D object
+          context.fill(new Path2D(stroke))
+          break;
+          default:
+        throw new Error (`Type not recognised ${element.type}`)
+    }
+  }
   
   useLayoutEffect(() => {
     const canvas = document.getElementById("canvas")
-    const ctx = canvas.getContext("2d")
-    //The clearRect() method clears the specified pixels within a given rectangle.
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    const context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
     const roughCanvas = rough.canvas(canvas)
-    elements.forEach(({roughElement}) => roughCanvas.draw(roughElement))
-  }, [elements])
+    elements.forEach(element => drawElement(roughCanvas, context, element))
+  }, [elements])//run if elements state changes
   
   useEffect(() => {
     const fill = document.getElementById("fill")
@@ -197,12 +267,17 @@ function ExcaliClone() {
 
       //return the element at the position we we clicked
       const element = getElementAtPosition(clientX, clientY, elements)
-      console.log(element)
       //if there is a element as the clicked position
       if(element){
-        const offsetX = clientX - element.x1
-        const offsetY = clientY - element.y1
-        setSelectedElement({...element, offsetX, offsetY})
+        if(element.type === "pencil"){
+          const xOffsets = element.points.map(point => clientX - point.x);
+          const yOffsets = element.points.map(point => clientY - point.y);
+          setSelectedElement({ ...element, xOffsets, yOffsets });
+        }else{
+          const offsetX = clientX - element.x1;
+          const offsetY = clientY - element.y1;
+          setSelectedElement({ ...element, offsetX, offsetY });
+        }
 
         if(element.position === 'inside') {
           setAction("moving")
@@ -231,21 +306,38 @@ function ExcaliClone() {
       }
     }else{
       const id = elements.length
-      console.log(elementType)
       const element = createElement(id,clientX,clientY,clientX,clientY, elementType);
-      
+      const elementFirstInstance = element
       //add new element in the elements state
       setElementState(prevState => [...prevState, element])
-      const data = {
-        id:id,
-        clientX:clientX,
-        clientY:clientY,
-        type:elementType,
-        fill:fill,
-        fillStyle:fillStyle,
-        strokeColor:strokeColor,
-        strokeWidth:strokeWidth
-      };
+      let data
+      if(element.type === "pencil"){
+        data = {
+          element:elementFirstInstance
+        }
+      }else{
+        data = {
+          id:id,
+          clientX:clientX,
+          clientY:clientY,
+          type:elementType,
+          fill:fill,
+          fillStyle:fillStyle,
+          strokeColor:strokeColor,
+          strokeWidth:strokeWidth
+        };
+      }
+      // console.log(element)
+      // const data = {
+      //   id:id,
+      //   clientX:clientX,
+      //   clientY:clientY,
+      //   type:elementType,
+      //   fill:fill,
+      //   fillStyle:fillStyle,
+      //   strokeColor:strokeColor,
+      //   strokeWidth:strokeWidth
+      // };
       //creating a custom like predefined events like click or mousemove and more
       const event = new CustomEvent("drawStart", { detail: data });
       //dispatching the event in document.documentElement where we listen for it in while recording
@@ -307,8 +399,6 @@ function ExcaliClone() {
   const handleMouseMove = (event) => {
     //coordinates while the mouse is moving
     const {clientX, clientY} = event
-
-    //if the action is 
     if(elementType === "selection"){
       const element = getElementAtPosition(clientX, clientY, elements)
       event.target.style.cursor = element ? cursorForPosition(element.position) : "default"
@@ -318,7 +408,13 @@ function ExcaliClone() {
     const index = elements.length-1
     const {x1,y1} = elements[index]
     updateElement(index,x1,y1, clientX, clientY, elementType, fill, fillStyle, strokeColor,strokeWidth)
-    const data = {
+    let data
+    if(elements[index].type === "pencil"){
+      data={
+        element:elements[index]
+      }
+    }else{
+      data = {
         id:index,
         x1:x1,
         y1:y1,
@@ -329,37 +425,49 @@ function ExcaliClone() {
         fillStyle:fillStyle,
         strokeColor:strokeColor,
         strokeWidth:strokeWidth
-      };
+      }
+    }
+    if(elements[index].type !== "pencil"){
       //creating a custom like predefined events like click or mousemove and more
       const event = new CustomEvent("drawing", { detail: data });
       //dispatching the event in document.documentElement where we listen for it in while recording
       document.documentElement.dispatchEvent(event);
+    }
     }else if(action==="moving"){
-      const  {id ,x1, x2, y1, y2, type, offsetX, offsetY} = selectedElement
-      const width = x2-x1
-      const height = y2-y1
-
-      //this is done to make sure x1 and y1 don't get changed to where mousedown event was performed
-      const newX1 = clientX - offsetX
-      const newY1 = clientY - offsetY
-      updateElement(id ,newX1, newY1,newX1 + width, newY1+height, type, fill, fillStyle, strokeColor,strokeWidth)
-      const data = {
-        id:id,
-        newX1:newX1,
-        newY1:newY1,
-        newX2:newX1+width,
-        newY2:newY1+height,
-        type:type,
-        fill:fill,
-        fillStyle:fillStyle,
-        strokeColor:strokeColor,
-        strokeWidth:strokeWidth
-      };
-      //creating a custom like predefined events like click or mousemove and more
-      const event = new CustomEvent("moving", { detail: data });
-      //dispatching the event in document.documentElement where we listen for it in while recording
-      document.documentElement.dispatchEvent(event);
-
+      if(selectedElement.type === "pencil"){
+        const newPoints = selectedElement.points.map((_, index) => ({
+          x: clientX - selectedElement.xOffsets[index],
+          y: clientY - selectedElement.yOffsets[index],
+        }));
+        const elementsCopy = [...elements];
+        elementsCopy[selectedElement.id].points = newPoints
+        setElementState(elementsCopy, true);
+      }else{
+        const  {id ,x1, x2, y1, y2, type, offsetX, offsetY} = selectedElement
+        const width = x2-x1
+        const height = y2-y1
+  
+        //this is done to make sure x1 and y1 don't get changed to where mousedown event was performed
+        const newX1 = clientX - offsetX
+        const newY1 = clientY - offsetY
+        updateElement(id ,newX1, newY1,newX1 + width, newY1+height, type, fill, fillStyle, strokeColor,strokeWidth)
+        const data = {
+          id:id,
+          newX1:newX1,
+          newY1:newY1,
+          newX2:newX1+width,
+          newY2:newY1+height,
+          type:type,
+          fill:fill,
+          fillStyle:fillStyle,
+          strokeColor:strokeColor,
+          strokeWidth:strokeWidth
+        };
+        //creating a custom like predefined events like click or mousemove and more
+        const event = new CustomEvent("moving", { detail: data });
+        //dispatching the event in document.documentElement where we listen for it in while recording
+        document.documentElement.dispatchEvent(event);  
+      }
     }else if(action === "resize"){
       const  {id ,type,position, ...coordiantes} = selectedElement
       const {x1,y1,x2,y2} = resizedCoordinates(clientX, clientY, position, coordiantes)
@@ -378,15 +486,17 @@ function ExcaliClone() {
 
       const event = new CustomEvent("resizing", {detail:data})
       document.documentElement.dispatchEvent(event)
-      updateElement(id,x1,y1,x2,y2,type, fill, fillStyle, stroke,strokeWidth)
+      updateElement(id,x1,y1,x2,y2,type, fill, fillStyle, strokeColor,strokeWidth)
     }
   }
+
+  const adjustmentRequired = type => ["line", "rectangle","circle"].includes(type);
 
   const handleMouseUp = () => {
     if(selectedElement){
       const index = selectedElement.id
       const {id, type} = elements[index]
-      if(action === "drawing"){
+      if ((action === "drawing" || action === "resizing") && adjustmentRequired(type)) {
         const {x1,y1,x2,y2} = adjustElementCorrdinates(elements[index])
         updateElement(id, x1,y1,x2,y2,type, fill, fillStyle, strokeColor,strokeWidth)
         const data = {
@@ -425,9 +535,10 @@ function ExcaliClone() {
         {/*ELLIPSE NOT IMPLEMENTED NOW WILL BE IMPLEMENTED IN FUTURE*/}
         {/* <input type="radio" id="ellipse" checked={elementType==="ellipse"} onChange={() => setTool("ellipse")}/>
         <label htmlFor="arrow">Ellipse </label> */}
+        <input type="radio" id="pencil" checked={elementType==="pencil"} onChange={() => setTool("pencil")}/>
+        <label htmlFor="pencil">Pencil </label>
         <div className="styleCard">
         <div>
-        Fill
         <select id="fill">
           <option>transparent</option>
           <option>red</option>
