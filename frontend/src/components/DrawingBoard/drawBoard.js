2533b62f33c668c036ed5faf570f973b5b9de84c
Aman <54789531+eppisai@users.noreply.github.com>
2021-10-09 17:51:14 +0530

    canvas css and width, height in drawing board

--- frontend/src/components/DrawingBoard/drawBoard.js
+++ frontend/src/components/DrawingBoard/drawBoard.js
@@ -117,7 +117,7 @@ function DrawingBoard() {
         //think of the two points forming a line then check how far is the user click
         return onLine(point.x, point.y,nextPoint.x,nextPoint.y, x,y) !== null
       })
-      return betweenAnyPoint ?"inside": null
+      return betweenAnyPoint ? "inside" : null
     }
   }
 
@@ -510,7 +510,7 @@ function DrawingBoard() {
               <div className="shapesOptions">
               
                  <label className = "tools" htmlFor="selection"><input type="radio" id="selection" checked = {elementType==="selection"} onChange={() => setTool("selection")}/>
-                 <svg viewBox="0 0 320 512" class=""><path d="M302.189 329.126H196.105l55.831 135.993c3.889 9.428-.555 19.999-9.444 23.999l-49.165 21.427c-9.165 4-19.443-.571-23.332-9.714l-53.053-129.136-86.664 89.138C18.729 472.71 0 463.554 0 447.977V18.299C0 1.899 19.921-6.096 30.277 5.443l284.412 292.542c11.472 11.179 3.007 31.141-12.5 31.141z"></path></svg>
+                 <svg viewBox="0 0 320 512" ><path d="M302.189 329.126H196.105l55.831 135.993c3.889 9.428-.555 19.999-9.444 23.999l-49.165 21.427c-9.165 4-19.443-.571-23.332-9.714l-53.053-129.136-86.664 89.138C18.729 472.71 0 463.554 0 447.977V18.299C0 1.899 19.921-6.096 30.277 5.443l284.412 292.542c11.472 11.179 3.007 31.141-12.5 31.141z"></path></svg>
                  </label>
                 
 
@@ -594,10 +594,10 @@ function DrawingBoard() {
         </div>
       </div>
      
-     <main>
+     <main style = {{ position : "absolute", top : "0" }}>
       <canvas id="canvas" 
-      width={window.innerWidth} 
-      height={window.innerHeight}
+      width = {window.innerWidth}
+      height = {window.innerHeight}
       onMouseDown = {handleMouseDown}
       onMouseMove = {handleMouseMove}
       onMouseUp = {handleMouseUp}
