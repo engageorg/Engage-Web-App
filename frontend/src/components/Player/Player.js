e08acc1bea8d98d1fb37ae3a992b6940371ac1c4
Aman <54789531+eppisai@users.noreply.github.com>
2021-10-16 13:01:59 +0530

    preloader changed

--- frontend/src/components/Player/Player.js
+++ frontend/src/components/Player/Player.js
@@ -12,48 +12,12 @@ import DrawingBoardPlayer from "../DrawingBoard/index";
 function Preloader(){
   return(
     <div className = "loader">
-    <div className ="preloader">
-	<div className ="box">
-		<div className ="box__inner">
-			<div className ="box__back-flap"></div>
-			<div className ="box__right-flap"></div>
-			<div className ="box__front-flap"></div>
-			<div className ="box__left-flap"></div>
-			<div className ="box__front"></div>
-		</div>
-	</div>
-	<div className ="box">
-		<div className ="box__inner">
-			<div className ="box__back-flap"></div>
-			<div className ="box__right-flap"></div>
-			<div className ="box__front-flap"></div>
-			<div className ="box__left-flap"></div>
-			<div className ="box__front"></div>
-		</div>
-	</div>
-	<div className ="line">
-		<div className ="line__inner"></div>
-	</div>
-	<div className ="line">
-		<div className ="line__inner"></div>
-	</div>
-	<div className ="line">
-		<div className ="line__inner"></div>
-	</div>
-	<div className ="line">
-		<div className ="line__inner"></div>
-	</div>
-	<div className ="line">
-		<div className ="line__inner"></div>
-	</div>
-	<div className ="line">
-		<div className ="line__inner"></div>
-	</div>
-	<div className ="line">
-		<div className ="line__inner"></div>
-	</div>
-</div>
-</div>
+       <div className ="preloader">
+        <div class="load">
+	         <img src="https://i.ibb.co/bP83CgK/spinner-PNG14.png" border="0"/>
+        </div>
+       </div>
+    </div>
   )
 }
 
