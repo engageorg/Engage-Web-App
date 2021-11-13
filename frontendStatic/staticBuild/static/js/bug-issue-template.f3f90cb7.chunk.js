2da66bb449c5e20e9b04de17dfdd422357e647d7
Aman <54789531+eppisai@users.noreply.github.com>
2021-11-13 11:08:12 +0530

    static build

---
+++ frontendStatic/staticBuild/static/js/bug-issue-template.f3f90cb7.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpengagev2=this.webpackJsonpengagev2||[]).push([[2],{328:function(n,e,t){"use strict";t.r(e),e.default=function(n){return"\n### Scene content\n\n```\nPaste scene content here\n```\n\n### Sentry Error ID\n\n".concat(n,"\n")}},330:function(n,e,t){"use strict";t.r(e),e.default=function(n){return"\n### Scene content\n\n```\nPaste scene content here\n```\n\n### Sentry Error ID\n\n".concat(n,"\n")}}}]);
+//# sourceMappingURL=bug-issue-template.f3f90cb7.chunk.js.map
