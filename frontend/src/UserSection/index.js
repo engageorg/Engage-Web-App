92a2f06974a0a101f0ca2363baad7951f4635432
Aman <54789531+eppisai@users.noreply.github.com>
2021-11-16 23:37:38 +0530

    UI/UX hardcoated

--- frontend/src/UserSection/index.js
+++ frontend/src/UserSection/index.js
@@ -1,227 +1,97 @@
 import React from "react";
 import "./styles.css";
-
+import { Link } from "react-router-dom"
 function UserSection(props) {
   return (
     <section className = "usersection">
       
-<main class="main">
-	<div class="responsive-wrapper">
-		<div class="main-header">
+<main className="main">
+	<div className="responsive-wrapper">
+		<div className="main-header">
 			<h1>Engage</h1>
 		</div>
-		<div class="horizontal-tabs">
-			<a href="#" class="active">Applications</a>
-			<a href="#">Recorded Lectures</a>
+		<div className="horizontal-tabs">
+			<a href="#" className="active">Applications</a>
+			<a href="#">Watch Lectures</a>
 		</div>
-		<div class="content-header">
-			<div class="content-header-intro">
+		<div className="content-header">
+			<div className="content-header-intro">
 				<h2>Intergrations and connected apps</h2>
 				<p>Supercharge your Productivity.</p>
 			</div>
-			<div class="content-header-actions">
-				<a href="#" class="button">
-					<i class="ph-faders-bold"></i>
-					<span>Filters</span>
+			<div className="content-header-actions">
+                <a href="#" className="button">
+					<span>Live Classes</span>
 				</a>
-				<a href="#" class="button">
-					<i class="ph-plus-bold"></i>
+                <Link to = "/recordform">
+				<div className="button">
 					<span>Record A Lecture</span>
-				</a>
+				</div>
+                </Link>
 			</div>
 		</div>
-		<div class="content">
-			<div class="content-panel">
-				<div class="vertical-tabs">
-					<a href="#" class="active">View all</a>
-					<a href="#">Teaching Assistance</a>
+		<div className="content">
+			<div className="content-panel">
+				<div className="vertical-tabs">
+					<a href="#" className="active">View all</a>
+					<a href="#">Teaching tools</a>
 					<a href="#">Computer Science</a>
 					<a href="#">Chemistry</a>
 					<a href="#">Physics</a>
 				</div>
 			</div>
-			<div class="content-main">
-				<div class="card-grid">
-					<article class="card">
-						<div class="card-header">
+			<div className="content-main">
+				<div className="card-grid">
+					<article className="uscard">
+						<div className="card-header">
 							<div>
 								<span><img src="https://assets.codepen.io/285131/zeplin.svg" /></span>
-								<h3>Zeplin</h3>
+								<h3>Chalk Board</h3>
 							</div>
 
 						</div>
-						<div class="card-body">
-							<p>Collaboration between designers and developers.</p>
+						<div className="card-body">
+							<p>Custom ChalkBoard specially designed for teaching</p>
 						</div>
-						<div class="card-footer">
+						<div className="card-footer">
 							<a href="#">View integration</a>
 						</div>
 					</article>
-					<article class="card">
-						<div class="card-header">
+
+
+                    <article className="uscard">
+						<div className="card-header">
 							<div>
-								<span><img src="https://assets.codepen.io/285131/github.svg" /></span>
-								<h3>GitHub</h3>
+								<span><img src="https://assets.codepen.io/285131/zeplin.svg" /></span>
+								<h3>Web Development IDE</h3>
 							</div>
 
 						</div>
-						<div class="card-body">
-							<p>Link pull requests and automate workflows.</p>
+						<div className="card-body">
+							<p>Great Web Development for beginners, that works on vscode editor</p>
 						</div>
-						<div class="card-footer">
+						<div className="card-footer">
 							<a href="#">View integration</a>
 						</div>
 					</article>
-					<article class="card">
-						<div class="card-header">
+
+
+                    <article className="uscard">
+						<div className="card-header">
 							<div>
-								<span><img src="https://assets.codepen.io/285131/figma.svg" /></span>
-								<h3>Figma</h3>
+								<span><img src="https://assets.codepen.io/285131/zeplin.svg" /></span>
+								<h3>C++ IDE</h3>
 							</div>
 
 						</div>
-						<div class="card-body">
-							<p>Embed file previews in projects.</p>
+						<div className="card-body">
+							<p>Learn DSA in C++, using the c++ compiler and IDE</p>
 						</div>
-						<div class="card-footer">
+						<div className="card-footer">
 							<a href="#">View integration</a>
 						</div>
 					</article>
-					<article class="card">
-						<div class="card-header">
-							<div>
-								<span><img src="https://assets.codepen.io/285131/zapier.svg" /></span>
-								<h3>Zapier</h3>
-							</div>
 
-						</div>
-						<div class="card-body">
-							<p>Build custom automations and integrations with apps.</p>
-						</div>
-						<div class="card-footer">
-							<a href="#">View integration</a>
-						</div>
-					</article>
-					<article class="card">
-						<div class="card-header">
-							<div>
-								<span><img src="https://assets.codepen.io/285131/notion.svg" /></span>
-								<h3>Notion</h3>
-							</div>
-
-						</div>
-						<div class="card-body">
-							<p>Embed notion pages and notes in projects.</p>
-						</div>
-						<div class="card-footer">
-							<a href="#">View integration</a>
-						</div>
-					</article>
-					<article class="card">
-						<div class="card-header">
-							<div>
-								<span><img src="https://assets.codepen.io/285131/slack.svg" /></span>
-								<h3>Slack</h3>
-							</div>
-
-						</div>
-						<div class="card-body">
-							<p>Send notifications to channels and create projects.</p>
-						</div>
-						<div class="card-footer">
-							<a href="#">View integration</a>
-						</div>
-					</article>
-					<article class="card">
-						<div class="card-header">
-							<div>
-								<span><img src="https://assets.codepen.io/285131/zendesk.svg" /></span>
-								<h3>Zendesk</h3>
-							</div>
-
-						</div>
-						<div class="card-body">
-							<p>Link and automate Zendesk tickets.</p>
-						</div>
-						<div class="card-footer">
-							<a href="#">View integration</a>
-						</div>
-					</article>
-					<article class="card">
-						<div class="card-header">
-							<div>
-								<span><img src="https://assets.codepen.io/285131/jira.svg" /></span>
-								<h3>Atlassian JIRA</h3>
-							</div>
-
-						</div>
-						<div class="card-body">
-							<p>Plan, track, and release great software.</p>
-						</div>
-						<div class="card-footer">
-							<a href="#">View integration</a>
-						</div>
-					</article>
-					<article class="card">
-						<div class="card-header">
-							<div>
-								<span><img src="https://assets.codepen.io/285131/dropbox.svg" /></span>
-								<h3>Dropbox</h3>
-							</div>
-
-						</div>
-						<div class="card-body">
-							<p>Everything you need for work, all in one place.</p>
-						</div>
-						<div class="card-footer">
-							<a href="#">View integration</a>
-						</div>
-					</article>
-					<article class="card">
-						<div class="card-header">
-							<div>
-								<span><img src="https://assets.codepen.io/285131/google-chrome.svg" /></span>
-								<h3>Google Chrome</h3>
-							</div>
-
-						</div>
-						<div class="card-body">
-							<p>Link your Google account to share bookmarks across your entire team.</p>
-						</div>
-						<div class="card-footer">
-							<a href="#">View integration</a>
-						</div>
-					</article>
-					<article class="card">
-						<div class="card-header">
-							<div>
-								<span><img src="https://assets.codepen.io/285131/discord.svg" /></span>
-								<h3>Discord</h3>
-							</div>
-
-						</div>
-						<div class="card-body">
-							<p>Keep in touch with your customers without leaving the app.</p>
-						</div>
-						<div class="card-footer">
-							<a href="#">View integration</a>
-						</div>
-					</article>
-					<article class="card">
-						<div class="card-header">
-							<div>
-								<span><img src="https://assets.codepen.io/285131/google-drive.svg" /></span>
-								<h3>Google Drive</h3>
-							</div>
-
-						</div>
-						<div class="card-body">
-							<p>Link your Google account to share files across your entire team.</p>
-						</div>
-						<div class="card-footer">
-							<a href="#">View integration</a>
-						</div>
-					</article>
 				</div>
 			</div>
 		</div>
