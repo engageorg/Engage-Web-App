ada1755c7c8c1df011f0db2615e96293b227ad17
Aman <54789531+eppisai@users.noreply.github.com>
2021-11-30 03:38:18 +0530

    static

---
+++ frontendStatic/static/js/bug-issue-template.7146ea6c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpengagev2=this.webpackJsonpengagev2||[]).push([[0],{361:function(n,e,t){"use strict";t.r(e),e.default=function(n){return"\n### Scene content\n\n```\nPaste scene content here\n```\n\n### Sentry Error ID\n\n".concat(n,"\n")}}}]);
+//# sourceMappingURL=bug-issue-template.7146ea6c.chunk.js.map
