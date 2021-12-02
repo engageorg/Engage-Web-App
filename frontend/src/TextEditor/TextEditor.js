c73a5b527a00d99c12e1837523b0b30030f73a62
Aman <54789531+eppisai@users.noreply.github.com>
2021-12-02 13:44:29 +0530

    implemented typing check inspired by live server extension's code

--- frontend/src/TextEditor/TextEditor.js
+++ frontend/src/TextEditor/TextEditor.js
@@ -27,9 +27,14 @@ function TextEditor(props) {
   }
   
   useEffect(() => {
+      let timer;              
+      const waitTime = 500; //in ms
       dispatch(setSrcDocs());
       document.getElementsByClassName("code_text")[0].addEventListener("keyup", function () {
-        dispatch(setSrcDocs());
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+          dispatch(setSrcDocs());
+      }, waitTime);
       });
       document.getElementsByClassName("closechalkboard")[0].addEventListener("click", function () {
         document.getElementsByClassName("chalkboardweb")[0].style.display = "none"
@@ -67,7 +72,7 @@ function TextEditor(props) {
     });
     
 
-  });
+  }, [srcDoc]);
 
 
   return (
