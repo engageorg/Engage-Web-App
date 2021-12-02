03302f8e5cd099a4f9539a7208ccf1b8204311eb
Aman <54789531+eppisai@users.noreply.github.com>
2021-12-02 22:21:46 +0530

    partial changes added

--- frontend/src/TextEditor/TextEditor.js
+++ frontend/src/TextEditor/TextEditor.js
@@ -189,7 +189,11 @@ function TextEditor(props) {
            <span className = "footer_text l_footer warnings"><i className="fas fa-exclamation-triangle"></i> {"  "}0</span>
        </div>
        <div className = "side_footer">
-           <span className = "footer_text r_footer"><i class="fas fa-terminal"></i>{" "}Console</span>
+           <span className = "footer_text r_footer">Layout: US</span>
+           <span className = "footer_text r_footer"><i className="fab fa-markdown"></i> HTML</span>
+           <span className = "footer_text r_footer">CRLF</span>
+           <span className = "footer_text r_footer">UTF-8</span>
+           <span className = "footer_text r_footer"><i className="fas fa-terminal"></i> {" "}Console</span>
        </div>
   
       </footer>
